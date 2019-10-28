1b228537a5073da8e7535e390dc309bef697a820
Timothy Carter (RIT Student) <txc1321@ad.rit.edu>
2019-10-28 15:07:13 -0400

    DomoMaker C

--- server/app.js
+++ server/app.js
@@ -8,6 +8,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -20,25 +23,52 @@ mongoose.connect(dbURL, (err) => {
   }
 });
 
+let redisURL = {
+  hostname: 'redis-17139.c57.us-east-1-4.ec2.cloud.redislabs.com',
+  port: 17139,
+};
+
+let redisPASS = 'H4YC3Zg5ii6na6GqEQrLFnySgaWNAokH';
+
+if(process.env.REDISCLOUD_URL){
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  redisPASS = redisURL.auth.split(':')[1];
+}
+
 const router = require('./router.js');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 app.use(session({
   key: 'sessionid',
+  store: new RedisStore({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    pass: redisPASS,
+  }),
   secret: 'Domo Arigato',
   resave: 'true',
   saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
+app.use(csrf());
+app.use((err, req, res, next) => {
+  if(err.code !== 'EBADCSRFTOKEN') return next(err);
+  console.log('Missing CSRF Token');
+  return false;
+})
 
 router(app);
 
--- server/router.js
+++ server/router.js
@@ -1,13 +1,14 @@
 const controllers = require('./controllers');
+const mid = require('./middleware');
 const router = (app) => {
-  app.get('/login', controllers.Account.loginPage);
-  app.get('/signup', controllers.Account.signupPage);
-  app.get('/logout', controllers.Account.logout);
-  app.get('/maker', controllers.Domo.makerPage);
-  app.get('/', controllers.Account.loginPage);
-  app.post('/login', controllers.Account.login);
-  app.post('/signup', controllers.Account.signup);
-  app.post('/maker', controllers.Domo.make);
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
+  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.make);
 };
 
 module.exports = router;
